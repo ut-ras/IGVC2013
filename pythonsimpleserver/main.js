e195a1470d99d58e37e4d43873ab8b5ea3d78005
Lt. Commander Data <ras@enterpras.(none)>
2012-10-25 15:27:02 -0500

    cleaned up web portal

--- pythonsimpleserver/main.js
+++ pythonsimpleserver/main.js
@@ -1,16 +1,39 @@
 var url = "http://3xrp.localtunnel.com/android";
 
+function keypressed(event) {
+    var key = event.which;
+
+    var x = 0, y = 0;
+    if (key == "A".charCodeAt(0)) {
+        x = -127;
+    } else if (key == "W".charCodeAt(0)) {
+        y = 127;
+    } else if (key == "S".charCodeAt(0)) {
+        y = -127;
+    } else if (key == "D".charCodeAt(0)) {
+        x = 127;
+    } else {
+        return;
+    }
+
+    getFile(url+"?x="+x+"&y="+y); 
+}
+
 function handleClick(num) {
     for (var i = 0; i < 5; i++) {
+        var x = 0, y = 0;
+        
         if (num == 0) {
-            getFile(url+"?x="+0+"&y="+127);
+            y = 127;
         } else if (num == 1) {
-            getFile(url+"?x="+0+"&y="+(-127));
+            y = -127;
         } else if (num == 2) {
-            getFile(url+"?x="+(-127)+"&y="+0);
+            x = -127;
         } else if (num == 3) {
-            getFile(url+"?x="+(127)+"&y="+0);
+            x = 127;
         }
+
+        getFile(url+"?x="+x+"&y="+y);
     }
 }
 
