0ce49f7c732d2f98d7eea92ef7fd13fdc2cd6267
Lt. Commander Data <ras@enterpras.(none)>
2012-10-25 14:43:53 -0500

    added mjpeg server

--- pythonsimpleserver/main.js
+++ pythonsimpleserver/main.js
@@ -1,28 +1,16 @@
-var url = "http://43hu.localtunnel.com/android";
-
-window.onload = function() {
-    setInterval(updateImage, 500);
-}
-
-var count = 0;
-
-function updateImage() {
-	var child = document.getElementById("image");
-	child.setAttribute('src', 'test'+count+'.jpeg?stop_cache='+Math.random());
-    count = (count+1)%30;
-}
+var url = "http://3xrp.localtunnel.com/android";
 
 function handleClick(num) {
     for (var i = 0; i < 5; i++) {
-    if (num == 0) {
-        getFile(url+"?x="+0+"&y="+127);
-    } else if (num == 1) {
-        getFile(url+"?x="+0+"&y="+(-127));
-    } else if (num == 2) {
-        getFile(url+"?x="+(-127)+"&y="+0);
-    } else if (num == 3) {
-        getFile(url+"?x="+(127)+"&y="+0);
-    }
+        if (num == 0) {
+            getFile(url+"?x="+0+"&y="+127);
+        } else if (num == 1) {
+            getFile(url+"?x="+0+"&y="+(-127));
+        } else if (num == 2) {
+            getFile(url+"?x="+(-127)+"&y="+0);
+        } else if (num == 3) {
+            getFile(url+"?x="+(127)+"&y="+0);
+        }
     }
 }
 
