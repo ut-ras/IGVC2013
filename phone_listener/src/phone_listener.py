596d74e50711fd6f7d9e0a3db57ab6f504946d11
Granny <granny@doloRAS.(none)>
2012-11-20 03:20:32 -0600

    Created phone_listener, a node that spits out gps, compass and accelerometer data.

---
+++ phone_listener/src/phone_listener.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import socket
+import struct
+
+import roslib; roslib.load_manifest('phone_listener')
+import rospy
+
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Vector3
+
+class Gps:
+    sig = 'g'
+    topic = rospy.Publisher('phone/gps', Point)
+
+    def pub(self, data):
+        parsed = struct.unpack('!ddd',data[1:])
+
+        msg = Point()
+        msg.x = parsed[0]
+        msg.y = parsed[1]
+        msg.z = parsed[2]
+        self.topic.publish(msg)
+
+class Mag:
+    sig = 'm'
+    topic = rospy.Publisher('phone/mag', Vector3)
+
+    def pub(self, data):
+        parsed = struct.unpack('!fff',data[1:])
+
+        msg = Vector3()
+        msg.x = parsed[0]
+        msg.y = parsed[1]
+        msg.z = parsed[2]
+        self.topic.publish(msg)
+
+class Accel:
+    sig = 'a'
+    topic = rospy.Publisher('phone/accel', Vector3)
+
+    def pub(self, data):
+        parsed = struct.unpack('!fff',data[1:])
+
+        msg = Vector3()
+        msg.x = parsed[0]
+        msg.y = parsed[1]
+        msg.z = parsed[2]
+        self.topic.publish(msg)
+
+
+def main():
+    rospy.init_node('phone_listener')
+
+    sen = {s.sig: s for s in [Gps(), Mag(), Accel()]}
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('',11111))
+
+    rospy.loginfo("Phone Listener is running: " + str(sock.getsockname()))
+
+    while not rospy.is_shutdown():
+        data = sock.recv(32)
+
+        sen[data[0]].pub(data)
+
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+
