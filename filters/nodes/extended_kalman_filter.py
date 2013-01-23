bc54da2b000b485249b2f2804833110c347bfecb
Granny <granny@doloRAS.(none)>
2013-01-22 22:22:15 -0600

    adding ekf in case I screw something up

--- filters/nodes/extended_kalman_filter.py
+++ filters/nodes/extended_kalman_filter.py
@@ -14,6 +14,8 @@ from ocean_server_imu.msg import RawData
 from filters.msg import RotatedIMUData,EKFData
 
 
+USING_OLD_BAGGED_DATA = False
+
 OCEAN_SERVER_IMU_INDEX = 0
 ENCODERS_INDEX = 1
 GPS_INDEX = 2
@@ -37,9 +39,16 @@ def oceanserver_imu_callback(data):
 def encoders_imu_callback(data):
     # rospy.loginfo("Measure:\n%f\n%f", data.acceleration.x, data.acceleration.y)
 
-    measurement_vector = numpy.matrix(
+    if USING_OLD_BAGGED_DATA:
+        measurement_vector = numpy.matrix(
+                            [ [data.linear.x],
+                              [-data.angular.z] ] )
+    else:
+        measurement_vector = numpy.matrix(
                             [ [data.linear.x],
                               [data.angular.z] ] )
+
+
     kf.Step(ENCODERS_INDEX, measurement_vector)
 
 def gps_imu_callback(data):
@@ -63,6 +72,9 @@ class ExtendedKalmanFilter:
         self.R_arr = _R_arr                         # Estimated variances in measurements
         self.I = numpy.eye(_P.shape[0])             # Identity matrix
         self.prev_time = None
+
+    def GetCurrentCovMatrix(self):
+        return self.P
 
     def GetCurrentState(self):
         return self.state
@@ -156,7 +168,7 @@ def gps_observation_funct(state, dt):
 
     return numpy.matrix(\
     	  [ [x + (dt*v + 0.5*dt*dt*a)*math.cos(z + dt*w)],\
-          [y + (dt*v + 0.5*dt*dt*a)*math.sin(z + dt*w)]])
+            [y + (dt*v + 0.5*dt*dt*a)*math.sin(z + dt*w)]])
 
 def gps_jacobian_funct(state, dt):
     x = state[0, 0]
@@ -220,8 +232,8 @@ def create_EKF():
     initial_probability = numpy.eye(8)
     process_covariance = numpy.eye(8)*1e-3
 
-    os_imu_measurement_covariance = numpy.eye(5)*0.0005
-    encoders_measurement_covariance = numpy.eye(2)*1e-12
+    os_imu_measurement_covariance = numpy.eye(5)*1e-6
+    encoders_measurement_covariance = numpy.eye(2)*1e-6
     gps_measurement_covariance = numpy.eye(2)*0.03
 
     return ExtendedKalmanFilter(transition_funct,\
@@ -233,23 +245,35 @@ def create_EKF():
                                 process_covariance,\
                                 [os_imu_measurement_covariance, encoders_measurement_covariance, gps_measurement_covariance])
 
-def create_msg(belief):
+def create_msg(belief, covariances):
     msg = EKFData()
 
-    msg.x_pos = belief[0][0]
-    msg.y_pos = belief[1][0]
-    msg.linear_velocity = belief[3][0]
-    msg.linear_acceleration = belief[5][0]
+    msg.header.stamp = rospy.get_rostime()
 
-    msg.roll = belief[6][0]
-    msg.pitch = belief[7][0]
-    msg.yaw = belief[2][0]
-    msg.yaw_rate = belief[4][0]
+    msg.x_pos = belief[0,0]
+    msg.y_pos = belief[1,0]
+    msg.linear_velocity = belief[3,0]
+    msg.linear_acceleration = belief[5,0]
+
+    msg.roll = belief[6,0]
+    msg.pitch = belief[7,0]
+    msg.yaw = belief[2,0]
+    msg.yaw_rate = belief[4,0]
+
+    msg.variances.x_pos = covariances[0,0]
+    msg.variances.y_pos = covariances[1,1]
+    msg.variances.linear_velocity = covariances[3,3]
+    msg.variances.linear_acceleration = covariances[5,5]
+
+    msg.variances.roll = covariances[6,6]
+    msg.variances.pitch = covariances[7,7]
+    msg.variances.yaw = covariances[2,2]
+    msg.variances.yaw_rate = covariances[4,4]
 
     return msg
 
 if __name__ == '__main__':
-    rospy.init_node('extended_kalman_filter', anonymous=True)
+    rospy.init_node('extended_kalman_filter', anonymous=False)
 
     kf = create_EKF()
 
@@ -264,7 +288,7 @@ if __name__ == '__main__':
         # rospy.loginfo("predicting & publishing!")
 
         kf.Predict()
-        pub.publish(create_msg(kf.GetCurrentState()))
+        pub.publish(create_msg(kf.GetCurrentState(), kf.GetCurrentCovMatrix()))
 
         r.sleep()
 
