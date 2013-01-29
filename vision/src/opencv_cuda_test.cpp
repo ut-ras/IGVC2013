006084ec62ea47af3ccc89060c32d5da83122230
Granny <granny@doloRAS.(none)>
2013-01-29 16:22:41 -0600

    added opencv_cuda test

--- vision/src/opencv_cuda_test.cpp
+++ vision/src/opencv_cuda_test.cpp
@@ -11,11 +11,8 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/gpu/gpu.hpp"
 
-int main (int argc, char* argv[])
-{
-    ros::init(argc, argv, "image_converter");
-    try
-    {
+void test_cuda() {
+    try {
         cv::Mat src_host = cv::imread("TESTIMAGE.jpg", CV_LOAD_IMAGE_GRAYSCALE);
         cv::gpu::GpuMat dst, src;
         src.upload(src_host);
@@ -23,14 +20,38 @@ int main (int argc, char* argv[])
         cv::gpu::threshold(src, dst, 128.0, 255.0, CV_THRESH_TOZERO);
 
         cv::Mat result_host = (cv::Mat) dst;
+        
+        std::cout << "cuda works!" << std::endl;
+
         cv::imshow("Result", result_host);
         cv::waitKey();
-    }
-    catch(const cv::Exception& ex)
-    {
+    } catch(const cv::Exception& ex) {
         std::cout << "Error: " << ex.what() << std::endl;
     }
-    return 0;
+}
+
+void test_opencv() {
+    try {
+        cv::Mat dst,
+                src = cv::imread("TESTIMAGE.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+
+        cv::threshold(src, dst, 128.0, 255.0, CV_THRESH_TOZERO);
+        
+        std::cout << "opencv works!" << std::endl;
+
+        cv::imshow("Result", dst);
+        cv::waitKey();
+    } catch(const cv::Exception& ex) {
+        std::cout << "Error: " << ex.what() << std::endl;
+    }
+}
+
+int main (int argc, char* argv[])
+{
+    ros::init(argc, argv, "opencv_cuda_test");
+    
+    test_cuda();
+    test_opencv();
 }
 
 
