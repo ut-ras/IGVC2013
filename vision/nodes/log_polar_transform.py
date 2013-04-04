c6f0a38a0245fe9cc7236697a26dd553aec75ff7
Granny <granny@doloRAS.(none)>
2013-04-04 07:43:12 -0500

    consolidating vision operations into one file

--- vision/nodes/log_polar_transform.py
+++ vision/nodes/log_polar_transform.py
@@ -5,52 +5,12 @@ import numpy as np
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from vision_lib import homography_transform, log_polar_transform
 
 RATE = 10
 
 # for conversion of raw image topic data to OpenCV image data
 bridge = CvBridge()
-
-PERSPECTIVE_TRANSFORM = np.array(
-[[  1.78778569e+00,   9.19775038e-01,  -2.56236007e+02],
- [  2.24321245e-02,   2.98569665e+00,  -3.37687301e+02],
- [  8.04901494e-05,   2.72273026e-03,   1.00000000e+00]])
-def homography_transform(img):
-    return cv2.warpPerspective(
-            img,
-            PERSPECTIVE_TRANSFORM,
-            (img.shape[1], img.shape[0])
-            )
-
-"""
-TODO: THESE TWO CONSTANTS SHOULD BE IN THE PARAMETER SERVER; THEY WILL BE
-USED IN WHATEVER NODE PROCESSES THE LOGPOLAR TOPIC
-"""
-logscale = 20
-anglescale = 80
-
-def log_polar_transform(img):
-    center = (img.width/2, img.height)
-    maxradius = math.sqrt((img.width/2)**2 + (img.height)**2)
-    width = int(logscale*math.log(maxradius))
-
-    logpolar = cv.CreateImage(
-            (width, anglescale),
-            cv.IPL_DEPTH_8U,
-            3 # why is this need to be 3 here, but 1 in polar_processor.py?
-            )
-
-    cv.LogPolar(
-            img,
-            logpolar,
-            center,
-            logscale,
-            cv.CV_INTER_LINEAR + cv.CV_WARP_FILL_OUTLIERS
-            )
-
-    aligned = cv.GetSubRect(logpolar, (0, anglescale/2, width, anglescale/2))
-
-    return aligned
 
 initWindow = False
 def display(homography, log_polar):
