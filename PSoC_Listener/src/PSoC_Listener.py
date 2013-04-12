4dced366733a0ac941a70bede4ef19c9d3098174
Granny <granny@doloRAS.(none)>
2013-04-12 14:22:04 -0500

    frank is continuing to fix recent bugs in psoclistener

--- PSoC_Listener/src/PSoC_Listener.py
+++ PSoC_Listener/src/PSoC_Listener.py
@@ -6,8 +6,10 @@ from std_msgs.msg import String
 import serial
 import string
 import atexit
+import time
 
-ser = serial.Serial(port = rospy.get_param("port",'/dev/PSoC'), baudrate = 921600)
+port = rospy.get_param("port",'/dev/PSoC')
+ser = serial.Serial()
 
 @atexit.register
 def onExit():
@@ -23,30 +25,47 @@ def callback(data):
     ser.flushOutput()
 
 def psoc():
+    global ser
     rospy.init_node('PSoC_Listener')
-    rospy.loginfo( "PSoC Listener is running on " + ser.portstr )
     pub = rospy.Publisher('psoc_data', PSoC)
     sub = rospy.Subscriber('psoc_cmd', String, callback)
-    ser.write('>ETFM\n')
-    ser.flushOutput()
     while not rospy.is_shutdown():
-        ser.flushInput()
-        line = ser.readline()
-        tokens = string.split(line)
-        if(tokens[0] == '(:' and tokens[len(tokens)-1] == ':)'):
-            p = PSoC()
-            p.left_enc = int(tokens[1])
-            p.right_enc = int(tokens[2])
-            p.vel_v = int(tokens[3])
-            p.vel_w = int(tokens[4])
-            p.adc = int(tokens[5])
-            p.time = long(tokens[6])
-            p.rate = int(tokens[7])
-            pub.publish(p)
-            rospy.logdebug('Telemetry message: '+line)
-        else:
-            rospy.loginfo('Info message from PSoC: '+ line)
-
+        try:
+            ser = serial.Serial(port = port, baudrate = 921600)
+            ser.flush()
+            ser.write('>ETFM\n')
+            ser.flushOutput()
+            rospy.loginfo( "PSoC Listener is running on " + ser.portstr )
+            while not rospy.is_shutdown():
+                ser.flushInput()
+                line = ser.readline()
+                tokens = string.split(line)
+                if(tokens[0] == '(:' and tokens[len(tokens)-1] == ':)'):
+                    try:
+                        p = PSoC()
+                        p.left_enc = int(tokens[1])
+                        p.right_enc = int(tokens[2])
+                        p.vel_v = int(tokens[3])
+                        p.vel_w = int(tokens[4])
+                        p.adc = int(tokens[5])
+                        p.time = long(tokens[6])
+                        p.rate = int(tokens[7])
+                        pub.publish(p)
+                        rospy.logdebug('Telemetry message: '+line)
+                    except rospy.exceptions.ROSSerializationException as e:
+                        rospy.logwarn('PSoC Listener fucked up.')
+                        rospy.logwarn(e.message)
+                        rospy.logwarn(e.strerror)
+                else:
+                    rospy.loginfo('Info message from PSoC: '+ line)
+        except serial.serialutil.SerialException:
+            print "Disconnected... Reconnecting..."
+            time.sleep(1)
+        except OSError as e:
+            print "Shit's really fucked..."
+            print e.message,e.strerror
+        #finally:
+        #    ser.close()
 if __name__ == "__main__":
     try:
         psoc()
