2246ff4fd4ce9a732b56aa9db31d9f80c9ed6063
Granny <granny@doloRAS.(none)>
2013-04-18 08:45:48 -0500

    making it similar to arbitrate between orange and white detection

--- vision/nodes/white_detection.py
+++ vision/nodes/white_detection.py
@@ -14,21 +14,15 @@ def hsv_orange_red_threshold(input_image):
     blur_image = cv.CreateMat(input_image.rows,input_image.cols,cv.CV_8UC3)
     cv.Smooth(input_image,blur_image,cv.CV_BLUR, 10, 10)
     proc_image = cv.CreateMat(input_image.rows,input_image.cols,cv.CV_8UC3)
+
     cv.CvtColor(blur_image, proc_image, cv.CV_BGR2HSV)
     split_image = [cv.CreateMat(input_image.rows,input_image.cols,cv.CV_8UC1),cv.CreateMat(input_image.rows,input_image.cols,cv.CV_8UC1),cv.CreateMat(input_image.rows,input_image.cols,cv.CV_8UC1)]
     cv.Split(proc_image, split_image[0],split_image[1],split_image[2], None )
 
     thresh_0 = cv.CreateMat(input_image.rows,input_image.cols,cv.CV_8UC1)
-    thresh_1 = cv.CreateMat(input_image.rows,input_image.cols,cv.CV_8UC1)
-    thresh_2 = cv.CreateMat(input_image.rows,input_image.cols,cv.CV_8UC1)
-    red_orange = cv.CreateMat(input_image.rows,input_image.cols,cv.CV_8UC1)
-    cv.Threshold(split_image[1],thresh_0, 128,255,cv.CV_THRESH_BINARY) # > 50% saturation
-    cv.Threshold(split_image[0],thresh_1, 220,255,cv.CV_THRESH_BINARY) # > Purple
-    cv.Threshold(split_image[0],thresh_2, 10, 255,cv.CV_THRESH_BINARY_INV) # < Yellow-Orange
-    cv.Add(thresh_1,thresh_2,red_orange)
-    cv.And(red_orange,thresh_0,red_orange)
+    cv.Threshold(split_image[2], thresh_0, 60, 255, cv.CV_THRESH_BINARY) # > 80% Lum
 
-    return red_orange
+    return thresh_0
 
 initWindow = False
 def display(img):
@@ -53,7 +47,7 @@ def init():
     rospy.init_node('orange_red_thresholder')
 
     sub = rospy.Subscriber('usb_cam/image_raw', Image, callback)
-    pub = rospy.Publisher('image_orange', Image)
+    pub = rospy.Publisher('image_white', Image)
 
     r = rospy.Rate(RATE)
 
